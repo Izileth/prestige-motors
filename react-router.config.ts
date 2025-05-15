6ccc19144171c1bdcae3ca92c7f0d814b9e629e0
Kawã Kleber <kawaklebersc@gmail.com>
2025-05-14 22:31:51 -0300

    Update Routes

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,8 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  async prerender() {
-    return ["/", "/login", "/register"];
-  },
   ssr: true,
 } satisfies Config;
