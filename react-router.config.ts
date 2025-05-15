56d233f2f20fc6bade181908186e03892855b0ea
Kawã Kleber <kawaklebersc@gmail.com>
2025-05-14 22:16:17 -0300

    Conplete Navigation Functions

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,8 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  appDirectory: "app",
+  async prerender() {
+    return ["/", "/login", "/register"];
+  },
   ssr: true,
 } satisfies Config;
